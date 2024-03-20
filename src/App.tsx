47dbf5b222166d90569dcb26b4b14c874fb97db9
le0nRoch <113020714+le0nRoch@users.noreply.github.com>
2024-03-20 10:36:37 +0100

    generalized chakra toast options

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "./configs/chakra";
+import { theme, toastOptions } from "./configs/chakra";
 import { Router } from "./Router";
 import { AuthProvider } from "./contexts/auth";
 
 function App() {
 	return (
-		<ChakraProvider theme={theme}>
+		<ChakraProvider theme={theme} toastOptions={toastOptions}>
 			<AuthProvider>
 				<Router />
 			</AuthProvider>
--- src/configs/chakra.ts
+++ src/configs/chakra.ts
@@ -1,3 +1,11 @@
-import { extendTheme } from "@chakra-ui/react";
+import { type ToastProviderProps, extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({});
+
+export const toastOptions: ToastProviderProps = {
+  defaultOptions: {
+    duration: 5000,
+    isClosable: true,
+    position: "top-right",
+  },
+};
