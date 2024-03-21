a508c47cbafd3ff6885cde64adcd17eead0779bc
le0nRoch <113020714+le0nRoch@users.noreply.github.com>
2024-03-21 14:52:09 +0100

    improved auth toast

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,103 +1,107 @@
 import {
-	AuthErrorCodes,
-	type User,
-	signInWithPopup,
-	signOut,
-	getAuth,
-	signInAnonymously,
+  AuthErrorCodes,
+  type User,
+  signInWithPopup,
+  signOut,
+  getAuth,
+  signInAnonymously,
 } from "firebase/auth";
 import {
-	type PropsWithChildren,
-	createContext,
-	useEffect,
-	useState,
+  type PropsWithChildren,
+  createContext,
+  useEffect,
+  useState,
 } from "react";
 import { googleProvider } from "../configs/firebase";
 import { FirebaseError } from "firebase/app";
 import { useToast } from "@chakra-ui/react";
 
 interface AuthContextData {
-	loginWithGoogle: () => Promise<boolean>;
-	loginAnnonymously: () => Promise<boolean>;
-	logout: () => Promise<void>;
-	user: User | null;
+  loginWithGoogle: () => Promise<User | undefined>;
+  loginAnnonymously: () => Promise<User | undefined>;
+  logout: () => Promise<void>;
+  user: User | null;
 }
 
 export const AuthContext = createContext<AuthContextData>({
-	loginWithGoogle: async () => false,
-	loginAnnonymously: async () => false,
-	logout: async () => {},
-	user: null,
+  loginWithGoogle: async () => undefined,
+  loginAnnonymously: async () => undefined,
+  logout: async () => {},
+  user: null,
 });
 
 export function AuthProvider({ children }: PropsWithChildren<object>) {
-	const auth = getAuth();
-	const [user, setUser] = useState<User | null>(null);
-	const toast = useToast();
+  const auth = getAuth();
+  const [user, setUser] = useState<User | null>(null);
+  const toast = useToast();
 
-	function toastError(error: unknown) {
-		if (!(error instanceof FirebaseError)) {
-			toast({
-				status: "error",
-				title: "Unknown error",
-				description: "An unknown error occurred. Please try again later.",
-			});
-		} else {
-			toast({
-				status: "error",
-				title: "Error",
-				description: error.message,
-			});
-		}
-	}
+  function toastError(error: unknown) {
+    if (!(error instanceof FirebaseError)) {
+      toast({
+        status: "error",
+        title: "Unknown error",
+        description: "An unknown error occurred. Please try again later.",
+      });
+    } else {
+      toast({
+        status: "error",
+        title: "Error",
+        description: error.message,
+      });
+    }
+  }
 
-	useEffect(() => {
-		const unsubscribe = auth.onAuthStateChanged((user) => {
-			setUser(user);
-		});
-		return () => unsubscribe();
-	});
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+    return () => unsubscribe();
+  });
 
-	async function loginWithGoogle(): Promise<boolean> {
-		try {
-			await signInWithPopup(auth, googleProvider);
-			return true;
-		} catch (error: unknown) {
-			if (
-				error instanceof FirebaseError &&
-				error.code === AuthErrorCodes.POPUP_CLOSED_BY_USER
-			)
-				return false;
-			toastError(error);
-			return false;
-		}
-	}
+  async function loginWithGoogle(): Promise<User | undefined> {
+    try {
+      return (await signInWithPopup(auth, googleProvider)).user;
+    } catch (error: unknown) {
+      if (
+        error instanceof FirebaseError &&
+        error.code === AuthErrorCodes.POPUP_CLOSED_BY_USER
+      )
+        return;
+      toastError(error);
+    }
+  }
 
-	async function loginAnnonymously(): Promise<boolean> {
-		try {
-			await signInAnonymously(auth);
-			return true;
-		} catch (error: unknown) {
-			toastError(error);
-			return false;
-		}
-	}
+  async function loginAnnonymously(): Promise<User | undefined> {
+    try {
+      return (await signInAnonymously(auth)).user;
+    } catch (error: unknown) {
+      toastError(error);
+    }
+  }
 
-	async function logout(): Promise<void> {
-		await signOut(auth);
-		// TODO: toast goodbye
-	}
+  async function logout(): Promise<void> {
+    try {
+      await signOut(auth);
+      toast({
+        status: "info",
+        title: "Goodbye",
+        description: "You have been logged out.",
+      });
+    } catch (error: unknown) {
+      toastError(error);
+    }
+  }
 
-	return (
-		<AuthContext.Provider
-			value={{
-				loginWithGoogle,
-				loginAnnonymously,
-				user,
-				logout,
-			}}
-		>
-			{children}
-		</AuthContext.Provider>
-	);
+  return (
+    <AuthContext.Provider
+      value={{
+        loginWithGoogle,
+        loginAnnonymously,
+        user,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,69 +3,70 @@ import { FcGoogle } from "react-icons/fc";
 import { useState } from "react";
 import { useAuth } from "../hooks/contexts";
 import { useLocation, useNavigate } from "react-router-dom";
+import { User } from "firebase/auth";
 
 type AuthProcessingStates =
-	| null
-	| "processing-google-auth"
-	| "processing-anonymous-auth";
+  | null
+  | "processing-google-auth"
+  | "processing-anonymous-auth";
 
 export function LoginPage() {
-	const toast = useToast();
-	const navigate = useNavigate();
-	const state = useLocation().state as { from?: string };
-	const { loginWithGoogle, loginAnnonymously } = useAuth();
-	const [processing, setProcessing] = useState<AuthProcessingStates>(null);
+  const toast = useToast();
+  const navigate = useNavigate();
+  const state = useLocation().state as { from?: string };
+  const { loginWithGoogle, loginAnnonymously } = useAuth();
+  const [processing, setProcessing] = useState<AuthProcessingStates>(null);
 
-	async function handleGoogleLogin() {
-		setProcessing("processing-google-auth");
-		const loggedIn = await loginWithGoogle();
-		setProcessing(null);
-		if (loggedIn) handleSuccessfullLogin();
-	}
+  async function handleGoogleLogin() {
+    setProcessing("processing-google-auth");
+    const user = await loginWithGoogle();
+    setProcessing(null);
+    if (user) handleSuccessfullLogin(user);
+  }
 
-	async function handleAnnonymousLogin() {
-		setProcessing("processing-anonymous-auth");
-		const loggedIn = await loginAnnonymously();
-		handleSuccessfullLogin();
-		setProcessing(null);
-		if (loggedIn) handleSuccessfullLogin();
-	}
+  async function handleAnnonymousLogin() {
+    setProcessing("processing-anonymous-auth");
+    const user = await loginAnnonymously();
+    setProcessing(null);
+    if (user) handleSuccessfullLogin(user);
+  }
 
-	function handleSuccessfullLogin() {
-		toast({
-			title: "Logged in",
-			description: "You have been logged in.",
-			status: "success",
-		});
-		if (state?.from) return navigate(state.from, { replace: true });
-		navigate(-1);
-	}
+  function handleSuccessfullLogin(user: User) {
+    const name = user.isAnonymous ? "Annonymous" : user.displayName ?? "User";
+    toast({
+      title: "Logged in",
+      description: `Welcome, ${name}!`,
+      status: "success",
+    });
+    if (state?.from) return navigate(state.from, { replace: true });
+    navigate(-1);
+  }
 
-	return (
-		<VStack>
-			<Heading>Login</Heading>
-			<VStack width={"100%"} gap={5}>
-				<Button
-					width={"100%"}
-					colorScheme="teal"
-					leftIcon={<FcGoogle />}
-					isDisabled={!!processing}
-					isLoading={processing === "processing-google-auth"}
-					onClick={handleGoogleLogin}
-				>
-					Google
-				</Button>
-				<Button
-					width={"100%"}
-					variant="outline"
-					colorScheme="teal"
-					isDisabled={!!processing}
-					isLoading={processing === "processing-anonymous-auth"}
-					onClick={handleAnnonymousLogin}
-				>
-					Annonymus
-				</Button>
-			</VStack>
-		</VStack>
-	);
+  return (
+    <VStack>
+      <Heading>Login</Heading>
+      <VStack width={"100%"} gap={5}>
+        <Button
+          width={"100%"}
+          colorScheme="teal"
+          leftIcon={<FcGoogle />}
+          isDisabled={!!processing}
+          isLoading={processing === "processing-google-auth"}
+          onClick={handleGoogleLogin}
+        >
+          Google
+        </Button>
+        <Button
+          width={"100%"}
+          variant="outline"
+          colorScheme="teal"
+          isDisabled={!!processing}
+          isLoading={processing === "processing-anonymous-auth"}
+          onClick={handleAnnonymousLogin}
+        >
+          Annonymus
+        </Button>
+      </VStack>
+    </VStack>
+  );
 }
