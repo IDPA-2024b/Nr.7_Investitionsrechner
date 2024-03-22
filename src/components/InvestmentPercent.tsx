2f10482fb97877193f43cef69f87bf1edb005f38
David <bischof.david.db@gmail.com>
2024-03-22 06:35:14 +0100

    started doughnut chart

---
+++ src/components/InvestmentPercent.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Doughnut } from 'react-chartjs-2';
+
+export function InvestmentPercent({ percentages }) {
+    const data = {
+        labels: ['Red', 'Blue', 'Yellow'],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [300, 50, 100],
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+            ],
+            hoverOffset: 4
+        }]
+    };
+
+    return (
+        <div>
+            <Doughnut data={data} />
+        </div>
+    );
+}
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,12 @@
 import { InvestmentChart } from "../components/InvestmentChart";
 import LastInvestmentData from "../MockData/overalData.json"
+import InvestmentPercentageData from "../MockData/InvestedPercent.json"
+import { InvestmentPercent } from "../components/InvestmentPercent";
 export function DashboardPage() {
   return (
     <div>
       <p>Dashboard</p>
-      <InvestmentChart InvestmentData={LastInvestmentData}/>
+      <InvestmentChart InvestmentData={LastInvestmentData} />
     </div>
   );
 }
