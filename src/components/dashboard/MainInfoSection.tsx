f37d3d5f1e84940f4503126b5bf6a345fca66993
DavDevDav <bischof.david.db@gmail.com>
2024-03-29 20:10:45 +0100

    added the boxes for the info part but i dont like 🙁

--- src/components/dashboard/MainInfoSection.tsx
+++ src/components/dashboard/MainInfoSection.tsx
@@ -11,6 +11,7 @@ interface MainInfoSectionProps {
         amount: number;
     }[]
 }
+
 export function MainInfoSection({ LastInvestmentData, TypeDiversity }: MainInfoSectionProps) {
 
     return (
@@ -19,18 +20,41 @@ export function MainInfoSection({ LastInvestmentData, TypeDiversity }: MainInfoS
                 justifyContent={'space-between'}
                 flexWrap={'wrap'}
                 gap={4}
+                
+                h={'100%'}
+                w="100%" // Set the width of the outer Flex to 100%
             >
-                <Box
-                    backgroundColor={'#fff'}
-                    minW={'300px'}
-                    minH={'300px'}
-                    p={4}
-                    borderRadius="30px"
-                    outline={' 1px solid #e2e8f0'}
-                    px={10}
+                <Flex
+                    flexDir={"column"}
+                    justifyContent={'space-between'}
+                    gap={1}
+                    w="calc(33.33% - 1.33rem)" // Set the width of the inner Flex to 33.33% minus the gap
                 >
 
-                </Box>
+                    <Box
+                        backgroundColor={'#fff'}
+                        minW={'300px'}
+                        p={4}
+                        borderRadius="30px"
+                        outline={' 1px solid #e2e8f0'}
+                        px={10}
+                        h="calc(50% - 1rem)" // Set the height of the box to 50% minus the gap
+                        minH={'150px'}
+                    >
+                    </Box>
+
+                    <Box
+                        backgroundColor={'#fff'}
+                        minW={'300px'}
+                        p={4}
+                        borderRadius="30px"
+                        outline={' 1px solid #e2e8f0'}
+                        px={10}
+
+                        minH={'150px'}
+                    >
+                    </Box>
+                </Flex>
                 <Box
                     backgroundColor={'#fff'}
                     minW={'300px'}
@@ -39,8 +63,10 @@ export function MainInfoSection({ LastInvestmentData, TypeDiversity }: MainInfoS
                     borderRadius="30px"
                     outline={' 1px solid #e2e8f0'}
                     px={10}
+                    w="calc(33.33% - 1.33rem)" // Set the width of the box to 33.33% minus the gap
                 >
                 </Box>
+
 
                 <Box
                     backgroundColor={'#fff'}
