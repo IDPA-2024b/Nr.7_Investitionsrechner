0b21d611e31eda0892c76b0881efb17ad75aa866
DavDevDav <bischof.david.db@gmail.com>
2024-04-02 12:03:25 +0200

    fixed up the increas of market value so the invested amount is now not as profit marked but i gotta do some more testing

--- src/components/dashboard/MainLineChart.tsx
+++ src/components/dashboard/MainLineChart.tsx
@@ -1,26 +1,62 @@
 import { DateRange, LineChart } from "../charts/Line"
 import { Box, Flex, Select, Text } from "@chakra-ui/react"
 import { useEffect, useState } from "react"
-import {TriangleUpIcon, TriangleDownIcon} from '@chakra-ui/icons'
+import { TriangleUpIcon, TriangleDownIcon } from '@chakra-ui/icons'
 import { TextWithTooltip } from "../TextWithTooltip"
 import { theme } from './../../configs/chakra';
+import { Investment } from './../../types/investment';
 interface MainLineChartProps {
     LastInvestmentData: {
         date: string;
         value: number;
+    }[],
+    investments: {
+        Investment: Investment
     }[]
 }
-export function MainLineChart({LastInvestmentData }: MainLineChartProps) {
+export function MainLineChart({ LastInvestmentData, investments }: MainLineChartProps) {
     const [dateRange, setDateRange] = useState('last7days');
     const [firstValue, setFirstValue] = useState(0)
     const [lastValue, setLastValue] = useState(0)
     const [percentageGain, setPercentageGain] = useState(0)
     const [amountGain, setAmountGain] = useState(0)
     function receiveData(data1: number, data2: number) {
-        setFirstValue(data1);
+        setFirstValue(calculateAmountSpentInTimeRange(investments, dateRange as DateRange) || data1);
         setLastValue(data2);
     }
 
+    function calculateAmountSpentInTimeRange(investments: Investment[], dateRange: DateRange) {
+
+        const now = new Date();
+        const date = new Date();
+        switch (dateRange) {
+            case DateRange.Last7Days:
+                date.setDate(now.getDate() - 7);
+                break;
+            case DateRange.LastMonth:
+                date.setMonth(now.getMonth() - 1);
+                break;
+            case DateRange.LastYear:
+                date.setFullYear(date.getFullYear() - 1);
+                date.setMonth(0);
+                date.setDate(1);
+                break;
+            case DateRange.All:
+                date.setFullYear(1970);
+                break;
+        }
+        let totalPaied = 0
+        console.log("date", date)
+        const filteredInvestments = investments.filter((investment) => {
+            const investmentDate = new Date(investment.purchase.date);
+            if (investmentDate >= date) {
+                totalPaied += investment.purchase.price * investment.purchase.amount;
+            }
+        });
+        return totalPaied;
+    }
+    const tmp = calculateAmountSpentInTimeRange(investments, dateRange as DateRange)
+    console.log("tmp", tmp)
     function calculatePercentageChange(firstValue: number, lastValue: number) {
         if (firstValue === 0) {
             return 0;
@@ -49,27 +85,27 @@ export function MainLineChart({LastInvestmentData }: MainLineChartProps) {
                     gap={4}
                 >
                     <Flex
-                    flexDir={'column'}
-                    gap={4}
+                        flexDir={'column'}
+                        gap={4}
                     >
                         <Text
-                        fontSize={"2rem"}
-                        fontWeight={"bold"}
+                            fontSize={"2rem"}
+                            fontWeight={"bold"}
                         >
-                        <TextWithTooltip name={'Current Market Value'} text="IDK PLEASE SNJ HELP 😭 I DONT KNOW THE VOCI" />
+                            <TextWithTooltip name={'Current Market Value'} text="IDK PLEASE SNJ HELP 😭 I DONT KNOW THE VOCI" />
                         </Text>
                         <Text
-                        fontWeight={"bold"}
-                        fontSize={"2rem"}
+                            fontWeight={"bold"}
+                            fontSize={"2rem"}
                         >
                             ${lastValue.toLocaleString()}
                         </Text>
                         <Text
-                        fontSize={"md"}
-                        mt={-4}
-                        color={percentageGain < 0 ? 'red.400' : 'green'}
+                            fontSize={"md"}
+                            mt={-4}
+                            color={percentageGain < 0 ? 'red.400' : 'green'}
                         >
-                             {amountGain > 0 ? '+' : ''} ${amountGain.toLocaleString()} ({percentageGain.toLocaleString()}%) {percentageGain < 0 ? <TriangleDownIcon color={'red.400'} /> : <TriangleUpIcon color={'green'} />}
+                            {amountGain > 0 ? '+' : ''} ${amountGain.toLocaleString()} ({percentageGain.toLocaleString()}%) {percentageGain < 0 ? <TriangleDownIcon color={'red.400'} /> : <TriangleUpIcon color={'green'} />}
 
                         </Text>
                     </Flex>
@@ -98,8 +134,8 @@ export function MainLineChart({LastInvestmentData }: MainLineChartProps) {
                     date: v.date,
                     pricePerUnit: v.value
                 }))}
-                dateRange={dateRange as DateRange}
-                receiveData={receiveData}
+                    dateRange={dateRange as DateRange}
+                    receiveData={receiveData}
                 />
             </Box>
         </>
