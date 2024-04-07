882f16dd2f6d5051f8bbedbd94005a78f8f0a599
David <bischof.david.db@gmail.com>
2024-04-07 18:46:05 +0200

    first version of the email js contact form. logic is working perfectly

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,40 +1,148 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import emailjs from '@emailjs/browser';
-
+import {
+    FormControl,
+    FormLabel,
+    Input,
+    Textarea,
+    Button,
+    Text,
+    Flex,
+    Heading,
+    Box,
+    Container,
+    useBreakpointValue,
+} from '@chakra-ui/react';
 export function Contact() {
-    const form = useRef();
+    const formRef = useRef();
+    const flexDirection = useBreakpointValue({ base: 'column', md: 'row' });
+    const [formData, setFormData] = useState({
+        from_name: '',
+        from_email: '',
+        topic: '',
+        message: '',
+    });
+
+    const handleInputChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
 
     const sendEmail = (e) => {
         e.preventDefault();
-
+        console.log('Submitted Data:', formData);
         emailjs
-            .sendForm(import.meta.env.VITE_EMAILJS_SERVICE_ID, import.meta.env.VITE_EMAILJS_TEMPLATE_ID , form.current, import.meta.env.VITE_EMAILJS_PUBLIC_KEY)
+            .sendForm(
+                import.meta.env.VITE_EMAILJS_SERVICE_ID,
+                import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+                formRef.current, // Pass the formRef.current as the form parameter
+                import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+            )
             .then(
                 (result) => {
                     console.log(result.text + "123");
-                    form.current.reset();
+                    formRef.current.reset();
                 },
                 (error) => {
                     console.log(error.text);
                 }
             );
     };
+
+
+
+
     return (
         <>
-            <form ref={form} onSubmit={sendEmail} className="form-container">
-                <h3 className='FooterSubtitle'>Contact</h3>
+            <Container
+                maxW={"container.lg"}
+            >
 
-                <label className="label">Name</label>
-                <input type="text" name="from_name" required placeholder="John Doe" className="input-field" />
-                <label className="label">Email</label>
-                <input type="email" name="from_email" required placeholder="John@Doe.com" className="input-field" />
-                <label className="label" htmlFor="topic">Topic</label>
-                <input type="text" name="topic" required placeholder="Collaboration" className="input-field" />
-                <label className="label">Message</label>
-                <textarea name="message" required placeholder="I would like to..." className="input-field" />
-                <input type="submit" value="Send" className="submit-button" />
+                <Flex
+                    direction={flexDirection}
+                    alignItems="center"
+                    justifyContent="center"
+                >
+                    <Box width="50%">
+                        <Heading>
+                            Contact us if you have any questions!
+                        </Heading>
+                        <Box>
+                            <hr style={{ width: "50%", backgroundColor: "teal", height: "4px", border: "none", marginTop: "20px" }} />
+                        </Box>
+                        <Text maxW={"90%"}>
+                            Send us a message and we will get back to you as soon as possible 💯💯💯.
+                        </Text>
+                        <Box fontStyle={"italic"} mt={"30px"} display={useBreakpointValue({ base: 'none', md: 'block' })}>
+                            <Text>
+                                InView CH
+                            </Text>
+                            <Text>
+                                Rosenstrasse 1
+                            </Text>
+                            <Text>
+                                8400 Winterthur
+                            </Text>
+                            <Text
+                                textDecor={"underline"}
+                            >
+                                <a href="mailto:idpa.investment.dashboard@gmail.com">idpa.investment.dashboard@gmail.com</a>
+                            </Text>
+                        </Box>
 
-            </form>
+                    </Box>
+                    <form ref={formRef} onSubmit={sendEmail} style={{ width: "50%" }}>
+                        <FormControl>
+                            <FormLabel>Name</FormLabel>
+                            <Input
+                                type="text"
+                                name="from_name"
+                                required
+                                placeholder="John Doe"
+                                onChange={handleInputChange}
+                            />
+                        </FormControl>
+
+                        <FormControl>
+                            <FormLabel>Email</FormLabel>
+                            <Input
+                                //type="email"
+                                name="from_email"
+                                required
+                                placeholder="John@Doe.com"
+                                onChange={handleInputChange}
+                            />
+                        </FormControl>
+
+                        <FormControl>
+                            <FormLabel htmlFor="topic">Topic</FormLabel>
+                            <Input
+                                type="text"
+                                name="topic"
+                                required
+                                placeholder="Collaboration"
+                                onChange={handleInputChange}
+                            />
+                        </FormControl>
+
+                        <FormControl>
+                            <FormLabel>Message</FormLabel>
+                            <Textarea
+                                name="message"
+                                required
+                                placeholder="I would like to..."
+                                onChange={handleInputChange}
+                            />
+                        </FormControl>
+
+                        <Button type="submit" colorScheme="teal" width={"100%"}>
+                            Send
+                        </Button>
+                    </form>
+                </Flex>
+            </Container>
         </>
     )
 }
