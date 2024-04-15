ddc85aa16d9daf4e5c9b5069d2514dd594cd8630
le0nRoch <113020714+le0nRoch@users.noreply.github.com>
2024-04-15 19:12:20 +0200

    added missing database url

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -11,6 +11,7 @@ const firebaseConfig: FirebaseOptions = {
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
@@ -20,9 +21,9 @@ const app = initializeApp(firebaseConfig);
 // Initialize Auth
 export const googleProvider = new GoogleAuthProvider();
 // Initialize Realtime Database
-export const database = getDatabase(app); 
+export const realtimeDB = getDatabase(app); 
 
 // Initialize Firestore
-const db = getFirestore(app);
+const firestore = getFirestore(app);
 // users/<userId>/investments/<investmentId>
-export const userCollection = collection(db, "users");
+export const userCollection = collection(firestore, "users");
