f970ddde1bb1f21571e0f3d31d33db1f24446f4f
le0nRoch <113020714+le0nRoch@users.noreply.github.com>
2024-04-19 06:24:52 +0200

    added pwa compatability🤩

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+});
